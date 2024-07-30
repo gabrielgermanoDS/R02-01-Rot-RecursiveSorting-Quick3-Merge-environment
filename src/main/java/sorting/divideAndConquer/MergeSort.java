c7521c4e2f4b8e1e20a3790f2ea64c7d2f1dee80
GbrlGrmno <103541716+GbrlGrmno@users.noreply.github.com>
2024-07-30 10:12:29 -0300

    feat: Implementação Merge Sort

--- src/main/java/sorting/divideAndConquer/MergeSort.java
+++ src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -12,7 +12,59 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		if (leftIndex < 0 || rightIndex >= array.length) {
+			return;
+		}
+		
+		// Caso Base
+		if (leftIndex >= rightIndex) {
+			return;
+		}
+
+		int middleIndex = (leftIndex + rightIndex) / 2;
+
+		sort(array, leftIndex, middleIndex);
+		sort(array, middleIndex + 1, rightIndex);
+		merge(array, leftIndex, middleIndex, rightIndex);
+	}
+
+	public void merge(T[] array, int leftIndex, int middleIndex, int rightIndex) {
+
+		T[] helper = (T[]) new Comparable[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			helper[i] = array[i];
+		}
+
+		int i = leftIndex;
+		int j = middleIndex + 1;
+
+		int k = leftIndex;
+
+		while ((i <= middleIndex) && (j <= rightIndex)) {
+
+			if (helper[i].compareTo(helper[j]) <= 0) {
+				array[k] = helper[i];
+				i++;
+			} else {
+				array[k] = helper[j];
+				j++;
+			}
+
+			k++;
+		}
+
+		while (i <= middleIndex) {
+			array[k] = helper[i];
+			i++;
+			k++;
+		}
+
+		while (j <= rightIndex) {
+			array[k] = helper[j];
+			j++;
+			k++;
+		}
 	}
 }
