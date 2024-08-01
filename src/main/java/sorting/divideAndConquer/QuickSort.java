a14d04f7967b8b054a9f949ce5e801a12a99241d
GbrlGrmno <103541716+GbrlGrmno@users.noreply.github.com>
2024-08-01 20:51:42 -0300

    feat: Implementação Quick Sort

--- src/main/java/sorting/divideAndConquer/QuickSort.java
+++ src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -1,6 +1,7 @@
 package sorting.divideAndConquer;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
@@ -14,7 +15,35 @@ public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		if (leftIndex < 0 || rightIndex >= array.length) {
+			return;
+		}
+
+		if (leftIndex < rightIndex) {
+            
+            int pivotIndex = partition(array, leftIndex, rightIndex);
+            
+            sort(array, leftIndex, pivotIndex - 1);
+            sort(array, pivotIndex + 1, rightIndex);
+        }
 	}
+
+	private int partition(T[] array, int leftIndex, int rightIndex) {
+        
+        T pivot = array[rightIndex];
+        int i = leftIndex - 1;
+
+        for (int j = leftIndex; j < rightIndex; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                
+                Util.swap(array, i, j);
+            }
+        }
+
+        
+        Util.swap(array, i + 1, rightIndex);
+        return i + 1;
+    }
 }
